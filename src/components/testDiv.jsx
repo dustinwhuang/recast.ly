85dc30173354f4b4650c3a399f19c3ab8bdc9ead
Hack Reactor Student <student@hackreactor.com>
2017-10-26 18:58:19 -0700

    Add react-router

---
+++ src/components/testDiv.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var testDiv = () => (
+  <div>
+    <h1> SUCCESSFUL RENDERED ELEMENT </h1>
+  </div>
+);
+
+export default testDiv;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter as Router, Route, hashHistory, Switch } from 'react-router-dom'
 import jquery from '../node_modules/jquery/dist/jquery.js'
 import App from './components/App.jsx'
+// import App from './components/testDiv.jsx'
+import test from './components/testDiv.jsx'
 
-ReactDOM.render(<App searchYouTube={window.searchYouTube} />, document.getElementById('app'));
+ReactDOM.render(
+  (
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <App searchYouTube={searchYouTube}/>}/>
+        <Route path="/test" component={test} />
+      </Switch>
+    </Router>
+  ),
+  document.getElementById('app'));
+
+
+// <App searchYouTube={window.searchYouTube} />
+// <Router history={hashHistory}>
+//   <Route path="/" component={testDiv} />
+//   <Route path="/test" component={testDiv} />
+// </Router>
+
+//component={App} searchYouTube={window.searchYouTube}/>
